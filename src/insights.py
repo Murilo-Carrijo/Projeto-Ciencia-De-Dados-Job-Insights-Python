071bab7b77f9b042667ce6c3c0b1551b71fbbefc
murilo <murilocarrijoadm@gmail.com>
2022-08-04 23:17:57 -0300

    feat: requisito 7 - função filter_by_industry para retornar o a industria informada

--- src/insights.py
+++ src/insights.py
@@ -69,21 +69,11 @@ def get_unique_industries(path):
 
 
 def filter_by_industry(jobs, industry):
-    """Filters a list of jobs by industry
-
-    Parameters
-    ----------
-    jobs : list
-        List of jobs to be filtered
-    industry : str
-        Industry for the list filter
-
-    Returns
-    -------
-    list
-        List of jobs with provided industry
-    """
-    return []
+    list_filter = []
+    for job in jobs:
+        if job["industry"] == industry:
+            list_filter.append(job)
+    return list_filter
 
 
 def get_max_salary(path):
